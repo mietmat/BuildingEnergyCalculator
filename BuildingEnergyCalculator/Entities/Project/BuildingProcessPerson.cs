0a912dce4015bdcd91264831318b392e7917cb9d
mietmat <mateuszmietus94@gmail.com>
2024-03-19 11:12:27 +0100

    added buildingProcessPerson

---
+++ BuildingEnergyCalculator/Entities/Project/BuildingProcessPerson.cs
@@ -0,0 +1,14 @@
+﻿using BuildingEnergyCalculator.Enums;
+
+namespace BuildingEnergyCalculator.Entities.Project
+{
+    public class BuildingProcessPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public Occupation Occupation { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
